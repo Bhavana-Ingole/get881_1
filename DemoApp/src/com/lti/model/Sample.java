8ee360357c8b5bc8ddd357cc900a83fe0c1e9d9d
hp <hp@DESKTOP-P15VVN3>
2020-12-18 16:06:45 +0530

    First commit

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Sample {
+	
+	public void show()
+	{
+		System.out.println("Hello world");
+	}
+	
+	public void method()
+	{
+		System.out.println("Hii All....how are you doing");
+	}
+}
