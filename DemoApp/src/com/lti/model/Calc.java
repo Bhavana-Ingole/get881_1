27f0c9d4c82c30711e7f13f649c4aa07ae038dcc
Bhavana-Ingole <76148942+Bhavana-Ingole@users.noreply.github.com>
2020-12-18 16:47:43 +0530

    mul() method added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,9 +4,11 @@ public class Calc {
 
 	public void add(int num1,int num2)
 	{
-
 		System.out.println("Summation is : "+(num1+num2));
-
+	}
+	public void mul(int num1,int num2)
+	{
+		System.out.println("Multiplication is : "+(num1*num2));
 	}
 	
 	
